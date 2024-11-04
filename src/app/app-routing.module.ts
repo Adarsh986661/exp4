4a96ba86101875d6ecc74a61b308e53b0140ebca
Dileep9123 <gunduboinadileep@gmail.com>
2024-11-04 20:25:56 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { FacultyComponent } from './faculty/faculty.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'student-login', component: StudentComponent },
+  { path: 'faculty-login', component: FacultyComponent },
+  { path: '', redirectTo: '/student-login', pathMatch: 'full' }, // Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
